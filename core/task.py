c4905d93bd7fe2f8a0c6719f7be9a1b1599bd566
Alexius Academia <alexius.academia@gmail.com>
2019-06-07 00:59:46 +0800

    Removed argument task name.

--- core/task.py
+++ core/task.py
@@ -9,10 +9,11 @@ class Task:
     task_name = ''
     task_id = 0
 
-    def __init__(self, name):
+    def __init__(self):
         """
         Constructor specifying the name of the task.
         :param name:
         """
-        self.task_name = name
+        self.task_name = 'Unnamed Task'
         self.task_id = id_generator()
+
