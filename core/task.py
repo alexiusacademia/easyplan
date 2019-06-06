a59f504c7a0d3646453e81aff711eb0f76d2b356
Alexius Academia <alexius.academia@gmail.com>
2019-06-07 00:38:33 +0800

    Added constructor and id_generator.

--- core/task.py
+++ core/task.py
@@ -1,3 +1,18 @@
+import uuid
+
+
+def id_generator():
+    return uuid.uuid1()
+
+
 class Task:
-    def __init__(self):
-        pass
+    task_name = ''
+    task_id = 0
+
+    def __init__(self, name):
+        """
+        Constructor specifying the name of the task.
+        :param name:
+        """
+        self.task_name = name
+        self.task_id = id_generator()
