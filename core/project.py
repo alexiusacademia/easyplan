c542c986a685993809f7c5c80c44382152cf5210
Alexius Academia <alexius.academia@gmail.com>
2019-06-07 00:21:45 +0800

    Added 2 functions, add and remove of task.

--- core/project.py
+++ core/project.py
@@ -25,3 +25,24 @@ class Project:
 
     def __init__(self):
         pass
+
+    def add_task(self, task):
+        """
+        Add a task to the project. The task start day will be on the first day by default and the
+        user will have to move in to its appropriate location of by adding dependencies or dependents.
+        :param task: A task object containing all the necessary information.
+        :return:
+        """
+        self._tasks.append(task)
+
+    def remove_task(self, task):
+        """
+        Remove a specific task.
+        :param task:
+        :return: Returns true if the task is successfully deleted.
+        """
+        try:
+            self._tasks.remove(task)
+            return True
+        except ValueError:
+            return False
