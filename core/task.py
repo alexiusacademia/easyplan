353b8c01447bb6cbdb83185474048dc108c1e788
Alexius Academia <alexius.academia@gmail.com>
2019-06-07 01:06:12 +0800

    Changed return types to tuple to return a message as well.

--- core/task.py
+++ core/task.py
@@ -8,12 +8,26 @@ def id_generator():
 class Task:
     task_name = ''
     task_id = 0
+    duration = 0
+    start_day = 0
+    upstream = None
 
     def __init__(self):
-        """
-        Constructor specifying the name of the task.
-        :param name:
-        """
         self.task_name = 'Unnamed Task'
         self.task_id = id_generator()
 
+    def rename(self, new_name):
+        self.task_name = new_name
+
+    def set_duration(self, d):
+        self.duration = d
+
+    def set_start_day(self, s):
+        self.start_day = s
+
+    def set_upstream(self, task):
+        if isinstance(task, self):
+            self.upstream = task
+            return True, 'Upstream set.'
+        else:
+            return False, 'Task must be an instance of task.'
