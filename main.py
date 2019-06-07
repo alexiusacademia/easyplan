4e830945e2ad5dddcf01ae760e19eff29bdca1a1
Alexius Academia <alexius.academia@gmail.com>
2019-06-08 07:30:10 +0800

    Implementation sample of using task segment.

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import core.task as task
+from core.task_segment import TaskSegment
 from core.project import Project
 
 t = task.Task()
 t.task_name = 'Clearing'
+for ts in t.task_segments:
+    if isinstance(ts, TaskSegment):
+        print(ts.start, ts.duration)
+
+print('Duration of Clearing is: ' + str(t.get_duration()))
 
 p = Project()
 p.add_task(t)
