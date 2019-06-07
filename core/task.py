14bfe7986db23b9cbcc6c43d8c4a991230718287
alexiusacademia <alexius.academia@gmail.com>
2019-06-07 09:13:03 +0800

    Initial draft of splitting the task.

--- core/task.py
+++ core/task.py
@@ -1,4 +1,8 @@
+# Import built-in modules
 import uuid
+
+# Import project modules
+from core.task_segment import TaskSegment
 
 
 def id_generator():
@@ -38,12 +42,29 @@ class Task:
         else:
             return False, 'Task must be an instance of task.'
 
-    def split_task(self, task_part, left):
+    def split_task(self, task_segment, left):
         """
         Cut a task part into two.
-        :param task_part: The task segment to be cut (e.g. is a task is already splitted into two and
+        :param task_segment: The task segment to be cut (e.g. is a task is already splitted into two and
             the second segment is the one to be split, use task_segment[1])
         :param left: The duration of the left part of the split.
         :return:
         """
-        pass
+        total_duration = task_segment.duration
+        start = task_segment.start
+
+        # Check instance
+        if not isinstance(task_segment, TaskSegment):
+            return False, 'task_segment given is not an instance of TaskSegment class.'
+
+        # Check the task segment duration
+        if left >= total_duration:
+            return False, 'The split duration of one part is greater than or not applicable to the task segment.'
+
+        # Create two task segments
+        ts1 = TaskSegment(start, left)
+        ts2 = TaskSegment(left, total_duration - left)
+
+        # Replace the old task segment on the list
+        location = self.task_segments.index(task_segment)
+        self.task_segments[location:location+1] = ts1, ts2
