0cf8aaa5ae66b174d10c891235f0c74466c1b3a5
Alexius Academia <alexius.academia@gmail.com>
2019-06-08 23:47:04 +0800

    Added default values.

--- gui/gantt_chart/task_list.py
+++ gui/gantt_chart/task_list.py
@@ -9,13 +9,17 @@ class TaskListPane(wx.Panel):
 
         self.task_list_ctrl = wx.ListCtrl(
             self,
-            style=wx.LC_REPORT | wx.BORDER_SUNKEN
+            style=wx.LC_REPORT | wx.BORDER_SUNKEN | wx.LC_SINGLE_SEL | wx.LC_EDIT_LABELS | wx.LC_HRULES | wx.LC_VRULES
         )
 
         # Create the default columns
         self.task_list_ctrl.InsertColumn(0, 'Task', width=200)
-        self.task_list_ctrl.InsertColumn(1, 'Start', width=140)
-        self.task_list_ctrl.InsertColumn(2, 'Duration', width=140)
+        self.task_list_ctrl.InsertColumn(1, 'Start', width=60, format=wx.LIST_FORMAT_CENTRE)
+        self.task_list_ctrl.InsertColumn(2, 'Duration', width=60, format=wx.LIST_FORMAT_CENTRE)
+
+        self.task_list_ctrl.InsertItem(0, 'Untitled Task')
+        self.task_list_ctrl.SetItem(0, 1, str(0))
+        self.task_list_ctrl.SetItem(0, 2, str(1))
 
         sizer.Add(self.task_list_ctrl, pos=(0, 0),
                   flag=wx.EXPAND|wx.LEFT|wx.RIGHT|wx.TOP|wx.BOTTOM)
