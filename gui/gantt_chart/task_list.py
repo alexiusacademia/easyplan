28a18683bc4f40ef20e2e2e84753e76542304acf
Alexius Academia <alexius.academia@gmail.com>
2019-06-08 22:20:54 +0800

    Used the GridBagSizer

--- gui/gantt_chart/task_list.py
+++ gui/gantt_chart/task_list.py
@@ -9,13 +9,18 @@ class TaskListPane(wx.Panel):
 
         self.task_list_ctrl = wx.ListCtrl(
             self,
-            size=(-1, 100),
             style=wx.LC_REPORT | wx.BORDER_SUNKEN
         )
 
         # Create the default columns
-        self.task_list_ctrl.InsertColumn(0, 'Task', width=300)
+        self.task_list_ctrl.InsertColumn(0, 'Task', width=200)
         self.task_list_ctrl.InsertColumn(1, 'Start', width=140)
         self.task_list_ctrl.InsertColumn(2, 'Duration', width=140)
 
-        sizer.Add(self.task_list_ctrl, wx.GBPosition(0, 0))
+        sizer.Add(self.task_list_ctrl, pos=(0, 0),
+                  flag=wx.EXPAND|wx.LEFT|wx.RIGHT|wx.TOP|wx.BOTTOM)
+
+        sizer.AddGrowableRow(0)
+        sizer.AddGrowableCol(0)
+
+        self.SetSizer(sizer)
