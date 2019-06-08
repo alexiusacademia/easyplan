05b243467af441e81a4236c2366a53185d5069d5
Alexius Academia <alexius.academia@gmail.com>
2019-06-09 00:06:42 +0800

    Added new method for adding task.

--- gui/gantt_chart/task_list.py
+++ gui/gantt_chart/task_list.py
@@ -1,5 +1,9 @@
 import wx
 
+# Import project modules
+from core.task import Task
+from core.project import Project
+
 
 class TaskListPane(wx.Panel):
     def __init__(self, parent):
@@ -11,11 +15,15 @@ class TaskListPane(wx.Panel):
             self,
             style=wx.LC_REPORT | wx.BORDER_SUNKEN | wx.LC_SINGLE_SEL | wx.LC_EDIT_LABELS | wx.LC_HRULES | wx.LC_VRULES
         )
+        self.Bind(wx.EVT_LIST_ITEM_SELECTED, self.on_item_selected, self.task_list_ctrl)
 
         # Create the default columns
         self.task_list_ctrl.InsertColumn(0, 'Task', width=200)
         self.task_list_ctrl.InsertColumn(1, 'Start', width=60, format=wx.LIST_FORMAT_CENTRE)
         self.task_list_ctrl.InsertColumn(2, 'Duration', width=60, format=wx.LIST_FORMAT_CENTRE)
+
+        # Initialize project
+        # TODO: Incorporate the core and the gui
 
         self.task_list_ctrl.InsertItem(0, 'Untitled Task')
         self.task_list_ctrl.SetItem(0, 1, str(0))
@@ -28,3 +36,18 @@ class TaskListPane(wx.Panel):
         sizer.AddGrowableCol(0)
 
         self.SetSizer(sizer)
+
+    def on_item_selected(self, evt):
+        print(evt.GetText())
+
+    def add_task(self, task_object, *args):
+        """
+        Add a task to the specific row.
+        :param task_object:
+        :param row_index:
+        :return:
+        """
+        if len(args) > 0:
+            # The index is given so we insert the task on that index
+            index = args[0]
+
