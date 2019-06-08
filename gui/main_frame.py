159ecec6651bf8621f72f2494aee7d0b825d53fa
Alexius Academia <alexius.academia@gmail.com>
2019-06-08 20:00:33 +0800

    Used the TaskListPane.

--- gui/main_frame.py
+++ gui/main_frame.py
@@ -1,7 +1,30 @@
+# Import built-in modules
 import wx
+
+# Import project modules
+from .gantt_chart.task_list import TaskListPane
 
 
 class MainFrame(wx.Frame):
     def __init__(self):
-        super().__init__(parent=None, title='Test')
+        super().__init__(parent=None, title='EasyPlan')
+        self.init_ui()
         self.Show()
+
+    def init_ui(self):
+        splitter = wx.SplitterWindow(self)
+
+        left_pane = TaskListPane(splitter)
+        right_pane = TaskListPane(splitter)
+
+        splitter.SplitVertically(left_pane, right_pane)
+
+        sizer = wx.GridBagSizer(vgap=5, hgap=5)
+
+        sizer.Add(splitter, pos=(0, 0),
+                  flag=wx.EXPAND|wx.LEFT|wx.RIGHT|wx.TOP|wx.BOTTOM)
+
+        sizer.AddGrowableRow(0)
+        sizer.AddGrowableCol(0)
+
+        self.SetSizer(sizer)
