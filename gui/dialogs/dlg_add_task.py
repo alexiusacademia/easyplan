a23388ce9efbe1c42c8f6d366cf7520f1591d8f5
Alexius Academia <alexius.academia@gmail.com>
2019-06-09 14:59:18 +0800

    Initialize controls of add task dialog.

--- gui/dialogs/dlg_add_task.py
+++ gui/dialogs/dlg_add_task.py
@@ -3,4 +3,12 @@ import wx
 
 class AddTaskDialog(wx.Dialog):
     def __init__(self):
-        pass
+        self.panel = wx.Panel(parent=self)
+        self.sizer = wx.GridBagSizer(vgap=5, hgap=5)
+        self.panel.SetSizer(self.sizer)
+
+        label_task_name = wx.StaticText(label='Task Name')
+        self.sizer.Add(label_task_name, pos=(0, 0))
+
+        entry_task_name = wx.TextEntry()
+        self.sizer.Add(entry_task_name, pos=(0, 1))
