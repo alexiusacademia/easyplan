94a19838978c15b89b2008bd75f1cb3bd4980c2f
alexiusacademia <alexius.academia@gmail.com>
2019-06-11 15:12:21 +0800

    Applied some mixins.

--- gui/gantt_chart/task_list.py
+++ gui/gantt_chart/task_list.py
@@ -7,15 +7,16 @@ from core.task import Task
 from core.project import Project
 
 
-class WorkBreakdownStructure(wx.ListCtrl, listmix.TextEditMixin):
+class WorkBreakdownStructure(wx.ListCtrl, listmix.TextEditMixin, listmix.ListCtrlAutoWidthMixin):
     def __init__(self, parent, id=wx.ID_ANY, pos=wx.DefaultPosition, size=wx.DefaultSize, style=0):
         wx.ListCtrl.__init__(self, parent, id, pos, size, style)
         listmix.TextEditMixin.__init__(self)
+        listmix.ListCtrlAutoWidthMixin.__init__(self)
 
 
 class TaskListPane(wx.Panel):
     def __init__(self, parent, project):
-        wx.Panel.__init__(self, parent=parent, style=wx.HSCROLL | wx.VSCROLL)
+        wx.Panel.__init__(self, parent=parent, style=wx.WANTS_CHARS)
 
         self.project = project
         self.parent = parent
@@ -23,7 +24,12 @@ class TaskListPane(wx.Panel):
 
         self.task_list_ctrl = WorkBreakdownStructure(
             self,
-            style=wx.LC_REPORT | wx.BORDER_SUNKEN | wx.LC_SINGLE_SEL | wx.LC_EDIT_LABELS | wx.LC_HRULES | wx.LC_VRULES
+            style=wx.LC_REPORT |
+                  wx.BORDER_NONE |
+                  # wx.BORDER_SUNKEN |
+                  wx.LC_SINGLE_SEL |
+                  wx.LC_HRULES |
+                  wx.LC_VRULES
         )
         self.Bind(wx.EVT_LIST_ITEM_SELECTED, self.on_item_selected, self.task_list_ctrl)
         self.Bind(wx.EVT_LIST_END_LABEL_EDIT, self.on_end_editing, self.task_list_ctrl)
@@ -35,8 +41,10 @@ class TaskListPane(wx.Panel):
         self.task_list_ctrl.InsertColumn(3, 'Duration', width=60, format=wx.LIST_FORMAT_CENTRE)
         self.task_list_ctrl.InsertColumn(4, 'Predecessor', width=80, format=wx.LIST_FORMAT_CENTER)
 
+        self.set_column_sizes()
+
         sizer.Add(self.task_list_ctrl, pos=(0, 0),
-                  flag=wx.EXPAND|wx.LEFT|wx.RIGHT|wx.TOP|wx.BOTTOM)
+                  flag=wx.EXPAND | wx.LEFT | wx.RIGHT | wx.TOP | wx.BOTTOM)
 
         sizer.AddGrowableRow(0)
         sizer.AddGrowableCol(0)
@@ -71,11 +79,13 @@ class TaskListPane(wx.Panel):
         li = self.task_list_ctrl
         li.ClearAll()
 
-        li.InsertColumn(0, 'Task Id')
+        li.InsertColumn(0, 'Id')
         li.InsertColumn(1, 'Task Name', width=200)
         li.InsertColumn(2, 'Start', width=60, format=wx.LIST_FORMAT_CENTRE)
         li.InsertColumn(3, 'Duration', width=60, format=wx.LIST_FORMAT_CENTRE)
-        self.task_list_ctrl.InsertColumn(4, 'Predecessor', width=80, format=wx.LIST_FORMAT_CENTER)
+        li.InsertColumn(4, 'Predecessor', width=80, format=wx.LIST_FORMAT_CENTER)
+
+        self.set_column_sizes()
 
         index = 0
         for task in self.project.tasks:
@@ -87,6 +97,14 @@ class TaskListPane(wx.Panel):
                 li.SetItem(index, 4, str(task.predecessor))
 
             index += 1
+
+    def set_column_sizes(self):
+        self.task_list_ctrl.SetColumnWidth(0, 30)
+        self.task_list_ctrl.SetColumnWidth(1, 200)
+        self.task_list_ctrl.SetColumnWidth(2, 50)
+        self.task_list_ctrl.SetColumnWidth(3, 50)
+        self.task_list_ctrl.SetColumnWidth(4, 50)
+        self.task_list_ctrl.SetColumnWidth(5, 50)
 
 # TODO Create dialog for renaming task
 # TODO Create dialog for splitting
