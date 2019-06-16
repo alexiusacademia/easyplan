22933392e129a9d840a789a39bbfb93d49ac7423
Alexius Academia <alexius.academia@gmail.com>
2019-06-16 11:48:57 +0800

    Dialog for moving task segment

---
+++ gui/dialogs/dlg_move_task_segment.py
@@ -0,0 +1,95 @@
+import wx
+
+from ..gantt_chart.constants import *
+
+
+class MoveTaskSegmentDialog(wx.Dialog):
+    project = None
+    selected_task = None
+    selected_task_segment = None
+    parent = None
+
+    def __init__(self, parent):
+        wx.Dialog.__init__(self, parent)
+
+        self.SetTitle('Split Task')
+
+        self.project = parent.project
+        self.selected_task = self.project.selected_task
+        self.selected_task_segment = self.project.selected_task_segment
+        self.parent = parent
+
+        self.init_ui()
+
+    def init_ui(self):
+        main_sizer = wx.BoxSizer(wx.VERTICAL)
+
+        main_sizer.AddSpacer(10)
+
+        self.add_title(main_sizer)
+
+        main_sizer.AddSpacer(30)
+        self.add_input('Left Task Duration', main_sizer)
+        main_sizer.AddSpacer(30)
+
+        split_button = wx.Button(self, ID_OK, label='Split Task')
+        split_button.Bind(wx.EVT_BUTTON, self.on_split_clicked)
+        split_button.SetDefault()
+
+        btn_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        btn_sizer.AddSpacer(10)
+        btn_sizer.Add(split_button)
+
+        main_sizer.Add(btn_sizer)
+        main_sizer.AddSpacer(20)
+
+        self.SetSizerAndFit(main_sizer)
+
+    def add_input(self, text, main_sizer):
+        label = wx.StaticText(self, label=text)
+
+        sizer = wx.BoxSizer(wx.HORIZONTAL)
+
+        sizer.AddSpacer(10)
+        sizer.Add(label)
+        sizer.AddSpacer(10)
+        sizer.Add(wx.TextCtrl(self, name='task_segment_start', style=wx.ALIGN_CENTER_HORIZONTAL), flag=wx.EXPAND | wx.ALL)
+        sizer.AddSpacer(10)
+
+        main_sizer.Add(sizer, flag=wx.EXPAND | wx.ALL)
+
+    def add_title(self, main_sizer):
+        title = 'Move the task segment at a given day.'
+
+        label = wx.StaticText(self, label=title, style=wx.ALIGN_CENTER_HORIZONTAL)
+        font = wx.Font(12, wx.FONTFAMILY_DEFAULT, wx.ITALIC, wx.NORMAL)
+        label.SetFont(font)
+        label.SetForegroundColour((100, 100, 100))
+
+        sizer = wx.BoxSizer(wx.HORIZONTAL)
+
+        sizer.AddSpacer(10)
+        sizer.Add(label)
+        sizer.AddSpacer(10)
+
+        main_sizer.Add(sizer)
+
+    def on_split_clicked(self, event):
+        if self.IsModal():
+            # Handle the splitting
+            new_start_text = self.FindWindowByName('task_segment_start')
+
+            if isinstance(new_start_text, wx.TextCtrl):
+                new_start = new_start_text.GetLineText(0)
+                if new_start.isdigit():
+                    new_start = int(new_start)
+
+                    self.parent.right_pane.redraw()
+
+                    # Stop the modal state
+                    self.EndModal(event.GetEventObject().GetId())
+                else:
+                    print('Nope')
+
+        else:
+            self.Close()
