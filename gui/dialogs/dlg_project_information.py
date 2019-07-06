eb767440ed9229a6f58a02c2d2d9b085131eb19b
Alexius Academia <alexius.academia@gmail.com>
2019-07-06 09:46:04 +0800

    Added input for start date.

--- gui/dialogs/dlg_project_information.py
+++ gui/dialogs/dlg_project_information.py
@@ -1,7 +1,15 @@
 import wx
+from wx.adv import DatePickerCtrl
+
+INPUT_ROW_SPACER = 10
+INPUT_GROUP_PADDING = 20
+LABEL_INPUT_SPACER = 20
 
 
 class ProjectInformationDialog(wx.Dialog):
+
+    start_date = None
+
     def __init__(self, parent):
         wx.Dialog.__init__(self, parent)
 
@@ -20,10 +28,14 @@ class ProjectInformationDialog(wx.Dialog):
 
 
 class ProjectInformationNB(wx.Notebook):
+
+    start_date = None
+
     def __init__(self, parent):
         wx.Notebook.__init__(self, parent)
 
         self.AddPage(self.general_panel(), 'General Information')
+        self.start_date = parent.start_date
 
     def general_panel(self):
         panel = wx.Panel(self)
@@ -43,7 +55,7 @@ class ProjectInformationNB(wx.Notebook):
         project_title_sizer.AddSpacer(5)
 
         project_manager_sizer = wx.BoxSizer(wx.HORIZONTAL)
-        main_sizer.AddSpacer(10)
+        main_sizer.AddSpacer(INPUT_ROW_SPACER)
         main_sizer.Add(project_manager_sizer)
 
         label_project_manager = wx.StaticText(panel, label='Project Manager:', name='project_manager')
@@ -54,6 +66,29 @@ class ProjectInformationNB(wx.Notebook):
         project_manager_sizer.AddSpacer(20)
         project_manager_sizer.Add(entry_project_manager, 2)
         project_manager_sizer.AddSpacer(5)
+
+        minor_details_main_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        main_sizer.AddSpacer(INPUT_ROW_SPACER)
+        main_sizer.Add(minor_details_main_sizer, flag=wx.EXPAND | wx.TOP | wx.RIGHT | wx.BOTTOM | wx.LEFT)
+
+        left_minor_panel = wx.Panel(panel)
+        minor_details_main_sizer.AddSpacer(INPUT_GROUP_PADDING)
+        minor_details_main_sizer.Add(left_minor_panel, 1)
+
+        right_minor_panel = wx.Panel(panel)
+        minor_details_main_sizer.Add(right_minor_panel, 1)
+        minor_details_main_sizer.AddSpacer(INPUT_GROUP_PADDING)
+
+        sizer_start_date = wx.BoxSizer(wx.HORIZONTAL)
+        label_start_date = wx.StaticText(left_minor_panel, label='Start Date')
+        sizer_start_date.Add(label_start_date, 1)
+        entry_start_date = DatePickerCtrl(right_minor_panel, -1, style=wx.adv.DP_DROPDOWN)
+        if self.start_date is None:
+            entry_start_date.SetValue(wx.DateTime.Now())
+        else:
+            entry_start_date.SetValue(self.start_date)
+        sizer_start_date.AddSpacer(LABEL_INPUT_SPACER)
+        sizer_start_date.Add(entry_start_date, 1)
 
         panel.SetSizerAndFit(main_sizer)
 
